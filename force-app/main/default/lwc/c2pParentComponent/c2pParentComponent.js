3a07ac671abafe4911bf9db94dcdc530d46e2bcb
Ankit8430 <ankitgu8430@gmail.com>
2025-08-17 20:07:22 +0530

    17Aug2025-1

--- force-app/main/default/lwc/c2pParentComponent/c2pParentComponent.js
+++ force-app/main/default/lwc/c2pParentComponent/c2pParentComponent.js
@@ -1,3 +1,14 @@
 import { LightningElement } from 'lwc';
 
-export default class C2pParentComponent extends LightningElement {}
+export default class C2pParentComponent extends LightningElement {
+    showModal=false
+    msg
+    clickHandler(){
+        this.showModal=true
+    }
+
+    closeModal(event){
+        this.msg=event.detail
+        this.showModal=false
+    }
+}
